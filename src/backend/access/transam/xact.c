72a8ba08f5a56e32a3b642ca411038f37b50dae6
dilip <dilip@localhost.localdomain>
2017-12-07 11:19:04 +0530

    Fix defect in applying undo action in case of abort
    Reviewed by Kuntal Ghosh

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -3813,7 +3813,9 @@ UserAbortTransactionBlock(void)
 	}
 
 	/* execute the undo actions */
-	if (latest_urec_ptr)
+	if (latest_urec_ptr &&
+	   ((s->blockState == TBLOCK_INPROGRESS) ||
+		(s->blockState == TBLOCK_SUBINPROGRESS)))
 		execute_undo_actions(latest_urec_ptr, s->start_urec_ptr, true);
 }
 
@@ -4192,7 +4194,9 @@ RollbackToSavepoint(const char *name)
 			 BlockStateAsString(xact->blockState));
 
 	/* execute the undo actions */
-	if (latest_urec_ptr)
+	if (latest_urec_ptr &&
+	   ((s->blockState == TBLOCK_INPROGRESS) ||
+		(s->blockState == TBLOCK_SUBINPROGRESS)))
 		execute_undo_actions(latest_urec_ptr, xact->start_urec_ptr, false);
 }
 
