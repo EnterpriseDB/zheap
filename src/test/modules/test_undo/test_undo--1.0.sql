5d44b81eab636cb7f002704ca685af8a2800024b
Thomas Munro <thomas.munro@enterprisedb.com>
2018-08-06 11:14:07 +0530

    Add tests for the undo log manager.
    Provide a test module that exercises undolog.c and undofile.c.
    
    Author: Thomas Munro
    Discussion: https://postgr.es/m/CAEepm%3D2EqROYJ_xYz4v5kfr4b0qw_Lq_6Pe8RTEC8rx3upWsSQ%40mail.gmail.com

---
+++ src/test/modules/test_undo/test_undo--1.0.sql
@@ -0,0 +1,53 @@
+\echo Use "CREATE EXTENSION test_undo" to load this file. \quit
+
+CREATE FUNCTION undo_allocate(size int, persistence text)
+RETURNS text
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
+
+CREATE FUNCTION undo_advance(ptr text, size int, persistence text)
+RETURNS void
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
+
+CREATE FUNCTION undo_append(bytes bytea, persistence text)
+RETURNS text
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
+
+CREATE FUNCTION undo_append_transaction_header(xid xid, persistence text)
+RETURNS text
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
+
+CREATE FUNCTION undo_append_file(path text)
+RETURNS text
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
+
+CREATE FUNCTION undo_extract_file(path text, undo_ptr text, size int, persistence text)
+RETURNS void
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
+
+CREATE FUNCTION undo_dump(undo_ptr text, size int, persistence text)
+RETURNS void
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
+
+CREATE FUNCTION undo_discard(undo_ptr text)
+RETURNS void
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
+
+CREATE FUNCTION undo_is_discarded(undo_ptr text)
+RETURNS boolean
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
+
+CREATE FUNCTION undo_foreground_discard_test(loops int, size int, persistence text)
+RETURNS void
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
+
+
