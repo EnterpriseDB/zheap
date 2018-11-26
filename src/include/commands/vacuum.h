macdice/postgres

Two pass vacuum
We need vacuum in zheap for non-delete marked indexes, however, we can
use undo to reduce three-pass to two-pass vacuum.  When a row is
deleted, the vacuum will directly mark the line pointer as unused,
writing an undo record as it does, and then mark the corresponding
index entries as dead.  If vacuum fails midway through the undo can
ensure that changes to the heap page are rolled back.  If the vacuum
goes on to commit, we don't need to revisit the heap page after index
cleanup.

We must be careful about  TID reuse: we will only allow a TID to be
reused when the transaction that has marked it as unused has
committed. At that point, we can be assured that all the index
entries corresponding to dead tuples will be marked as dead.

Currently, due to lack of visibility map for zheap, we scan all the
pages during vacuum.  A future patch which will introduce visibility
map in zheap will remove that limitation.

Patch by me, Mithun has fixed few bugs and added implementation for
Rollback action, also he has done basic verification of the patch

src/include/commands/vacuum.h
@@ -14,6 +14,7 @@
 #ifndef VACUUM_H
 #define VACUUM_H
 
+#include "access/amapi.h"
 #include "access/htup.h"
 #include "catalog/pg_statistic.h"
 #include "catalog/pg_type.h"
@@ -147,6 +148,34 @@ typedef struct VacuumParams
 									 * to use default */
 } VacuumParams;
 
+typedef struct LVRelStats
+{
+	/* hasindex = true means two-pass strategy; false means one-pass */
+	bool		hasindex;
+	/* Overall statistics about rel */
+	BlockNumber old_rel_pages;	/* previous value of pg_class.relpages */
+	BlockNumber rel_pages;		/* total number of pages */
+	BlockNumber scanned_pages;	/* number of pages we examined */
+	BlockNumber pinskipped_pages;	/* # of pages we skipped due to a pin */
+	BlockNumber frozenskipped_pages;	/* # of frozen pages we skipped */
+	BlockNumber tupcount_pages; /* pages whose tuples we counted */
+	double		scanned_tuples; /* counts only tuples on tupcount_pages */
+	double		old_rel_tuples; /* previous value of pg_class.reltuples */
+	double		new_rel_tuples; /* new estimated total # of tuples */
+	double		new_dead_tuples;	/* new estimated total # of dead tuples */
+	BlockNumber pages_removed;
+	double		tuples_deleted;
+	BlockNumber nonempty_pages; /* actually, last nonempty page + 1 */
+	/* List of TIDs of tuples we intend to delete */
+	/* NB: this list is ordered by TID address */
+	int			num_dead_tuples;	/* current # of entries */
+	int			max_dead_tuples;	/* # slots allocated in array */
+	ItemPointer dead_tuples;	/* array of ItemPointerData */
+	int			num_index_scans;
+	TransactionId latestRemovedXid;
+	bool		lock_waiter_detected;
+} LVRelStats;
+
 /* GUC parameters */
 extern PGDLLIMPORT int default_statistics_target;	/* PGDLLIMPORT for PostGIS */
 extern int	vacuum_freeze_min_age;
@@ -189,6 +218,18 @@ extern void vacuum_delay_point(void);
 /* in commands/vacuumlazy.c */
 extern void lazy_vacuum_rel(Relation onerel, int options,
 				VacuumParams *params, BufferAccessStrategy bstrategy);
+extern void lazy_vacuum_index(Relation indrel, IndexBulkDeleteResult **stats,
+							  LVRelStats *vacrelstats);
+extern void lazy_cleanup_index(Relation indrel, IndexBulkDeleteResult *stats,
+							   LVRelStats *vacrelstats);
+extern void lazy_record_dead_tuple(LVRelStats *vacrelstats,
+					   ItemPointer itemptr);
+extern bool should_attempt_truncation(LVRelStats *vacrelstats);
+extern void lazy_truncate_heap(Relation onerel, LVRelStats *vacrelstats);
+
+/* in commands/zvacuumlazy.c */
+extern void lazy_vacuum_zheap_rel(Relation onerel, int options,
+					VacuumParams *params, BufferAccessStrategy bstrategy);
 
 /* in commands/analyze.c */
 extern void analyze_rel(Oid relid, RangeVar *relation, int options,

EnterpriseDB/zheap

src/include/commands/vacuum.h
@@ -14,6 +14,7 @@
 #ifndef VACUUM_H
 #define VACUUM_H
 
+#include "access/amapi.h"
 #include "access/htup.h"
 #include "catalog/pg_class.h"
 #include "catalog/pg_statistic.h"
@@ -148,6 +149,34 @@ typedef struct VacuumParams
 									 * to use default */
 } VacuumParams;
 
+typedef struct LVRelStats
+{
+	/* hasindex = true means two-pass strategy; false means one-pass */
+	bool		hasindex;
+	/* Overall statistics about rel */
+	BlockNumber old_rel_pages;	/* previous value of pg_class.relpages */
+	BlockNumber rel_pages;		/* total number of pages */
+	BlockNumber scanned_pages;	/* number of pages we examined */
+	BlockNumber pinskipped_pages;	/* # of pages we skipped due to a pin */
+	BlockNumber frozenskipped_pages;	/* # of frozen pages we skipped */
+	BlockNumber tupcount_pages; /* pages whose tuples we counted */
+	double		old_live_tuples;	/* previous value of pg_class.reltuples */
+	double		new_rel_tuples; /* new estimated total # of tuples */
+	double		new_live_tuples;	/* new estimated total # of live tuples */
+	double		new_dead_tuples;	/* new estimated total # of dead tuples */
+	BlockNumber pages_removed;
+	double		tuples_deleted;
+	BlockNumber nonempty_pages; /* actually, last nonempty page + 1 */
+	/* List of TIDs of tuples we intend to delete */
+	/* NB: this list is ordered by TID address */
+	int			num_dead_tuples;	/* current # of entries */
+	int			max_dead_tuples;	/* # slots allocated in array */
+	ItemPointer dead_tuples;	/* array of ItemPointerData */
+	int			num_index_scans;
+	TransactionId latestRemovedXid;
+	bool		lock_waiter_detected;
+} LVRelStats;
+
 /* GUC parameters */
 extern PGDLLIMPORT int default_statistics_target;	/* PGDLLIMPORT for PostGIS */
 extern int	vacuum_freeze_min_age;
@@ -194,6 +223,18 @@ extern Relation vacuum_open_relation(Oid relid, RangeVar *relation,
 /* in commands/vacuumlazy.c */
 extern void lazy_vacuum_rel(Relation onerel, int options,
 				VacuumParams *params, BufferAccessStrategy bstrategy);
+extern void lazy_vacuum_index(Relation indrel, IndexBulkDeleteResult **stats,
+							  LVRelStats *vacrelstats);
+extern void lazy_cleanup_index(Relation indrel, IndexBulkDeleteResult *stats,
+							   LVRelStats *vacrelstats);
+extern void lazy_record_dead_tuple(LVRelStats *vacrelstats,
+					   ItemPointer itemptr);
+extern bool should_attempt_truncation(LVRelStats *vacrelstats);
+extern void lazy_truncate_heap(Relation onerel, LVRelStats *vacrelstats);
+
+/* in commands/zvacuumlazy.c */
+extern void lazy_vacuum_zheap_rel(Relation onerel, int options,
+					VacuumParams *params, BufferAccessStrategy bstrategy);
 
 /* in commands/analyze.c */
 extern void analyze_rel(Oid relid, RangeVar *relation, int options,