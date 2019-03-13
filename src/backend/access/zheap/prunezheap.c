c79eba02208fb0e271e00f596ce78b2ea3b76caf
Beena Emerson <memissemerson@gmail.com>
2019-03-13 09:12:02 +0530

    Update Copyright date in zheap files to 2019

--- src/backend/access/zheap/prunezheap.c
+++ src/backend/access/zheap/prunezheap.c
@@ -3,7 +3,7 @@
  * prunezheap.c
  *	  zheap page pruning
  *
- * Portions Copyright (c) 1996-2017, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1996-2019, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
--- src/backend/postmaster/discardworker.c
+++ src/backend/postmaster/discardworker.c
@@ -3,7 +3,7 @@
  * discardworker.c
  *	  The undo discard worker for asynchronous undo management.
  *
- * Portions Copyright (c) 1996-2017, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1996-2019, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
