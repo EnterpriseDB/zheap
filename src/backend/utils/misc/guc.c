0d5ee4335f0cecd3a8d801a6760014b3ac8a2bb3
Thomas Munro <thomas.munro@gmail.com>
2019-05-10 13:40:47 +1200

    Set max_worker_processes default to 12.
    Otherwise parallel regression tests fail due to lack of worker slots.

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -2807,7 +2807,7 @@ static struct config_int ConfigureNamesInt[] =
 			NULL,
 		},
 		&max_worker_processes,
-		8, 0, MAX_BACKENDS,
+		12, 0, MAX_BACKENDS,
 		check_max_worker_processes, NULL, NULL
 	},
 
