4590900f7f44d74c0652565dff3a76db46146e22
Andres Freund <andres@anarazel.de>
2020-02-05 11:29:55 -0800

    undoxacttest: Show before/after contents in tests.
    This is done separately due to the verbosity.

--- src/test/regress/sql/undoxacttest.sql
+++ src/test/regress/sql/undoxacttest.sql
@@ -29,71 +29,98 @@ END
 $p$;
 
 -- implicit xact: single undo generating statement, statement succeeds
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 1);
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- implicit xact: single undo generating  statement, statement fails
 -- FIXME: this shows a WARNING indicating a bug
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 SELECT CASE WHEN undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 1) IS NOT NULL THEN raise_error() END;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: single undo generating statement, statement succeeds, followed by COMMIT
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 3);
 COMMIT;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: single undo generating statement, statement succeeds, followed by ROLLBACK
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 4);
 ROLLBACK;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: single undo generating statement, statement fails, followed by COMMIT
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT CASE WHEN undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 5) IS NOT NULL THEN raise_error() END;
 COMMIT;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: single undo generating statement, statement fails, followed by ROLLBACK
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT CASE WHEN undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 6) IS NOT NULL THEN raise_error() END;
 COMMIT;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 
 -- implicit xact: two undo generating statements, both succeed
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 7)\;SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 8);
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- implicit xact: two undo generating statements, both succeed, followed by erroring statement
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 9)\;SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 10);SELECT raise_error();
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- implicit xact: two undo generating statements, first succeeds, second fails
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 11)\;SELECT CASE WHEN undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 12) IS NOT NULL THEN raise_error() END;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 
 -- explicit xact: two undo generating statements, both succeed, followed by COMMIT
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 13);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 14);
 COMMIT;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: two undo generating statements, both succeed, followed by ROLLBACK
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 13);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 14);
 ROLLBACK;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: two undo generating statements, both succeed, followed by errror, followed by COMMIT
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 15);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 16);
 SELECT raise_error();
 COMMIT;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: two undo generating statements, both succeed, followed by errror, followed by ROLLBACK
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 17);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 18);
 SELECT raise_error();
 ROLLBACK;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 
 -- explicit xact: undo, savepoint, undo, rollback to savepoint, undo; commit;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 19);
 SAVEPOINT foo;
@@ -101,8 +128,10 @@ SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 20);
 ROLLBACK TO SAVEPOINT foo;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 21);
 COMMIT;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: undo, savepoint, undo, rollback to savepoint, undo; rollback;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 22);
 SAVEPOINT foo;
@@ -110,8 +139,10 @@ SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 23);
 ROLLBACK TO SAVEPOINT foo;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 24);
 ROLLBACK;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: undo, savepoint, undo, error, rollback to savepoint, undo; commit;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 25);
 SAVEPOINT foo;
@@ -120,8 +151,10 @@ SELECT raise_error();
 ROLLBACK TO SAVEPOINT foo;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 27);
 COMMIT;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: undo, savepoint, undo, error, rollback to savepoint, undo; rollback;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 25);
 SAVEPOINT foo;
@@ -130,42 +163,54 @@ SELECT raise_error();
 ROLLBACK TO SAVEPOINT foo;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 27);
 ROLLBACK;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: undo, savepoint, undo, commit;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 29);
 SAVEPOINT foo;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 29);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 30);
 ROLLBACK;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: undo, savepoint, undo, rollback;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 31);
 SAVEPOINT foo;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 32);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 33);
 ROLLBACK;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: undo, savepoint, undo, rollback;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 31);
 SAVEPOINT foo;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 32);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 33);
 ROLLBACK;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- implicit xact: create a fair bit of undo
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, g.i) FROM generate_series(1, 1000) g(i);
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: create a fair bit of undo, rollback
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, g.i) FROM generate_series(1, 1000) g(i);
 ROLLBACK;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 -- explicit xact: undo record set spanning logs, ROLLBACK
 -- FIXME: test with commit, once hack below not needed
 -- FIXME: this currently crashes
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 BEGIN;
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 35);
 
@@ -173,5 +218,6 @@ SELECT pg_force_truncate_undo_log(logno::int) FROM pg_stat_get_undo_logs() WHERE
 
 SELECT undoxacttest_fetch_and_inc('undoxacttest_perm'::regclass, 36);
 ROLLBACK;
+SELECT undoxacttest_read('undoxacttest_perm'::regclass);
 
 DROP TABLE undoxacttest_perm;
